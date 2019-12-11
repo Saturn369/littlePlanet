44d5b75dae3f8141420c0fb4b07f579950141b91
Saturn369 <freedomenka@gmail.com>
2019-12-11 19:01:38 +0000

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -138,4 +138,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'freedomenka@gmail.com'
-EMAIL_HOST_PASSWORD = 'Fuckgoogle@@#60'
+EMAIL_HOST_PASSWORD = '******'
